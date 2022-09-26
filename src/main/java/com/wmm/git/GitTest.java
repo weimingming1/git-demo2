fe293d9d724522346fe937a304296e1598e4b9d7
weimingming1 <z45648466@163.com>
2022-09-26 20:21:46 +0800

    third

--- src/main/java/com/wmm/git/GitTest.java
+++ src/main/java/com/wmm/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
+        System.out.println("git3");
     }
 }
