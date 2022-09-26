8c85ac51fa2160efcb9f0a3901b15c461f75125f
weimingming1 <58066405+weimingming1@users.noreply.github.com>
2022-09-26 21:09:03 +0800

    wmm first

--- src/main/java/com/wmm/git/GitTest.java
+++ src/main/java/com/wmm/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("gitefsefs5");
         System.out.println("git6ss");
         System.out.println("push");
+                System.out.println("push");
     }
 }
